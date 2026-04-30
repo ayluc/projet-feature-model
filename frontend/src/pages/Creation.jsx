d2ee522c6ee304bcbf5e1528ada3e71fc79c641d
Lucas <lucas.branchu@live.fr>
2026-04-30 10:02:05 +0200

    fix affichage sidebar + suppression de l'ancien fichier de la sidebar

--- frontend/src/pages/Creation.jsx
+++ frontend/src/pages/Creation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Sidebar from '@/components/Sidebar';
+import Sidebar from '@/components/FeatureModelEditor/SidebarReact';
 import FeatureModelEditor from '@/components/FeatureModelEditor/FeatureModelEditor';
 
 function Creation() {
