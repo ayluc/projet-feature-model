318567249af4a4add772bee4d81f633b7b5f5a5d
LauraChd <laura.chaduc@etud.univ-angers.fr>
2026-05-05 15:02:38 +0200

    CARDINALITE

--- frontend/src/components/nodes/NodeCardinalite.jsx
+++ frontend/src/components/nodes/NodeCardinalite.jsx
@@ -9,7 +9,7 @@ export function NodeCardinalite({data, isConnectable}) {
   return (
     <div className="node-cardinalite">
       <div style={{ textAlign: "center", display: 'flex', flexDirection: 'column'}}>
-        <label style={{ fontSize: "6px", fontWeight: "bold" }}>COMBINAISON</label>
+        <label style={{ fontSize: "6px", fontWeight: "bold" }}>CARDINALITE</label>
         <label>{`[${data?.cardinaliteMin}..${data?.cardinaliteMax}]`}</label>
         <Handle type="source" position={Position.Bottom} isConnectable={isConnectable}/>
         <Handle type="target" position={Position.Top} isConnectable={isConnectable}/>
